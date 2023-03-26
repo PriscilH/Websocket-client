d81a229b779e8956cfce80718ef3ab229fc710a1
Priscilla <comptepro@mbpdepriscilla.home>
2023-03-27 00:54:52 +0200

    install Gifted chat

--- App.js
+++ App.js
@@ -1,32 +1,58 @@
-// import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Platform, KeyboardAvoidingView } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+
+const user = {
+  _id: String(Math.random()),
+  name: "Farid Safi"
+};
 
 let ws = null;
 
-export default function App() {
-  useEffect(() => {
-    // Se connecter à un serveur
-     ws = new WebSocket("ws://localhost:8080");
-  }, []);
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button
-          title="Send message"
-          onPress={() => {
-            ws.send("Hello World");
-          }}
-        />
-    </View>
-  );
-}
+const Screen = () => {
+  const [messages, setMessages] = useState([]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    ws = new WebSocket("ws://localhost:8080");
+    return () => {
+      ws.close();
+      ws = null;
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleReceive = event => {
+      const message = JSON.parse(event.data);
+      setMessages([message, ...messages]);
+    };
+
+    if (ws) {
+      ws.addEventListener("message", handleReceive);
+    }
+    return () => {
+      if (ws) {
+        ws.removeEventListener("message", handleReceive);
+      }
+    };
+  }, [messages]);
+
+  return (
+    <>
+      <GiftedChat
+        messages={messages}
+        onSend={messagesToSend => {
+          const message = messagesToSend[0];
+          ws.send(JSON.stringify(message));
+          setMessages([message, ...messages]);
+        }}
+        user={user}
+      />
+      <KeyboardAvoidingView
+        behavior="padding"
+        enabled={Platform.OS === "android"}
+      />
+    </>
+  );
+};
+
+export default Screen;
